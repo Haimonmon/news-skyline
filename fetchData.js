330055774ae637d6eb0a47adc4e7820c23e14922
Haimonmon <beankor8@gmail.com>
2024-09-28 05:29:07 +0800

    Not Finished but will finish soon :sparkles:

--- fetchData.js
+++ fetchData.js
@@ -8,6 +8,13 @@ let fetchedData = {}
 //     "https://api.nytimes.com/svc/topstories/v2/idk.json?api-key=<api-key>"
 // ]
 
+// * For Testing ONly
+let apiURL = [
+    './example-api-data.json',
+    './arts.json',
+    './sports.json',
+    './world.json'
+]
 
 /**
  * ? Fetch all data at once for reuse , i come up for this to solve requesting again and again to their api
@@ -30,12 +37,12 @@ const fetchAllData = async () => {
     
         fetchedData = {
             homePageData: dataArray[0],
-            sportsPageData: dataArray[1],
-            artsPageData: dataArray[2],
+            artsPageData: dataArray[1],
+            sportsPageData: dataArray[2],
             worldPageData: dataArray[3]
         }
         
-        console.log(fetchedData)
+        return fetchedData
     } catch(error) {
         console.log(`Failed to fetch, Check your apiKey bro. ${error}`)
     }
@@ -51,10 +58,11 @@ const fetchData = async (filePath) => {
         const data = await response.json();
 
         if (data.status === 'OK') {
-            data.results.forEach(newsPosts => {
-                const newsPostTitle = newsPosts.title;
-                console.log(newsPostTitle);
-            });
+            fetchedData = {
+                homePageData: data
+            }
+
+            return fetchedData
         } else {
             console.log('Response didnt meet ERROR');
         }
@@ -76,9 +84,136 @@ const enableSearchBarFunctionality = () => {
         console.log(searchValue);
     })
 }
+
+
+const convertAPIDates = (apiDate) => {
+    const date = new Date(apiDate)
+
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  
+   
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June', 
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    
+    const dayOfWeek = weekdays[date.getDay()];
+    const month = months[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    return `${dayOfWeek}, ${month} ${day}, ${year}`;
+}
+
+const displayDateToday = (data) => {
+    const convertedFormalDate = convertAPIDates(data.last_updated)
+    
+    const dateTodayElement = document.getElementById('todays-date')
+
+    dateTodayElement.innerHTML = `<p> ${convertedFormalDate} <br> Today\'s Agenda <p>`;
+}
+
+const getAPIResult = (data) => {
+    return data.results
+}
+
+const displayDateTodayHotTopics = (data) => {
+    const dataResults = getAPIResult(data)
+
+    // ! Temporary News For now
+    const temporaryNewsPost = dataResults[0]
+
+    const hotTopicPublisher = document.getElementById('hot-topic-publisher');
+    hotTopicPublisher.innerHTML = ` <p id="publisher"> ${temporaryNewsPost.byline} </p> <hr> <p id="date-published">${convertAPIDates(temporaryNewsPost.published_date)}</p>`
+
+    const hotTopicTitle = document.getElementById('hot-topic-title')
+    hotTopicTitle.innerHTML = `<h1>${temporaryNewsPost.title}</h1>`
+
+    const hotTopicAbstract = document.getElementById('hot-topic-abstract')
+    hotTopicAbstract.innerHTML = `<p>${temporaryNewsPost.abstract}</p>`
+}
+
+const displayTopStories = (data, sectionName, startingAPIPostIndex, endingAPIPostIndex) => {
+    const dataResults = getAPIResult(data)
+
+    const mainContainer = document.getElementById(sectionName)
+
+    mainContainer.innerHTML = ''
+
+    for (let i = startingAPIPostIndex; i <= endingAPIPostIndex; i++) {
+
+        const images = dataResults[i].multimedia;
+        const imagePost = images[0].url || './assets/image/IMG-Header-Page-0001.jpg';
+
+        mainContainer.innerHTML += `
+        <a href="${dataResults[i].url}" target="_blank">
+            <div class="top-stories-container">
+                    <div class="information-container">
+                        <div id="top-stories-publisher">
+                            <p>${dataResults[i].byline}</p>
+                            <hr>
+                            <p>${convertAPIDates(dataResults[i].published_date)}</p>
+                        </div>
+                        <div id="top-stories-title">
+                            <h1>${dataResults[i].title}</h1>
+                        </div>
+                        <div id="top-stories-abstract">
+                            <p>${dataResults[i].abstract}</p>
+                        </div>
+
+                        <div class="top-stories-read-more-btn">
+                            <button>Read More</button>
+                        </div>
+                    </div>
+                    <img src="${imagePost}" alt="">
+                </div>
+        </a>
+        `
+    }
+}
+
+const displayOtherStories = (data, sectionName, startingAPIPostIndex, endingAPIPostIndex) => {
+    const dataResults = getAPIResult(data)
+
+    const mainContainer = document.getElementById(sectionName)
+
+    mainContainer.innerHTML = ''
+
+    for (let i = startingAPIPostIndex; i <= endingAPIPostIndex; i++) {
+        const images = dataResults[i].multimedia;
+        const imagePost = images[0].url || './assets/image/IMG-Header-Page-0001.jpg';
+
+        mainContainer.innerHTML += `
+            <a href="${dataResults[i].url}" target="_blank">
+                <div class="other-sports-news-container">
+                    <img src="${imagePost}" alt="">
+                    <h1>${dataResults[i].title}</h1>
+                    <p>${dataResults[i].abstract}</p>
+                    <img id="read-more" src="./assets/navigation-icons/Arrow 3.png" alt="">
+                </div>
+            </a>`;
+    }
+}
+
 const filePafh = './example-api-data.json';
 
-fetchData(filePafh);
-enableSearchBarFunctionality()
+// const filePafh = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=I3SCMGvGeTN6bzgOgpbFyDOaAcDIlOzL'
+
+const main = async () => {
+    const data = await fetchAllData()
+    console.log(data)
+    const homePageData = data.homePageData;
+    const artsPageData = data.artsPageData;
+    const sportsPageData = data.sportsPageData;
+    
+    displayDateToday(homePageData)
+    displayDateTodayHotTopics(homePageData)
+    displayTopStories(homePageData, 'top-stories1',4,7)
+    enableSearchBarFunctionality()
+    displayOtherStories(sportsPageData, 'sports-stories',0,2)
+    displayOtherStories(artsPageData, 'art-stories', 0, 2)
+}
+
+main()
 
 // * Run this with html to see on console
